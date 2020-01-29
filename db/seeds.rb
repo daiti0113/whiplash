ac16d870d87f0414addd70d2119966dba0ca6aaa
daiti0113 <stennis.ds.0113@gmail.com>
2020-01-29 23:45:18 +0900

    fix:fix seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,18 +1,22 @@
 require 'csv'
 
-CSV.foreach('csv/Pearl.csv', headers: true) do |row|
-    Item.create!(
-        created_at:     row['created_at'],
-        updated_at:     row['updated_at'],
-        manufacturer:   row['manufacturer'],
-        name:           row['name:'],
-        thickness:      row['thickness:'],
-        length:         row['length:'],
-        material:       row['material:'],
-        tip_material:   row['tip_material:'],
-        tip_shape:      row['tip_shape:'],
-        img_path:       row['img_path:'],
-        taper:          row['taper:'],
-        price:          row['price:']
-    )
+Dir.glob("csv/*.csv") do |path| 
+    puts "-----------------"
+    puts path
+    CSV.foreach(path, headers: true) do |row|
+        Item.create!(
+            created_at:     row['created_at'],
+            updated_at:     row['updated_at'],
+            manufacturer:   row['manufacturer'],
+            name:           row['name'],
+            thickness:      row['thickness'],
+            length:         row['length'],
+            material:       row['material'],
+            tip_material:   row['tip_material'],
+            tip_shape:      row['tip_shape'],
+            img_path:       row['img_path'],
+            taper:          row['taper'],
+            price:          row['price']
+        )
+    end
 end
